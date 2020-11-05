96e3400627d1c8aaa437c383d1e4d56ccf4c46a2
nckmightcs <cmcknight408@gmail.com>
2020-11-05 10:30:03 -0500

    added pictures of food.

--- Order/order-script.js
+++ Order/order-script.js
@@ -22,42 +22,7 @@ function selectFoodItem (itemId) {
         var index = foodItems.indexOf(checkbox.value);
         foodItems.splice(index, 1);
         console.log(foodItems);
+        var item = document.getElementById('total');
+        item[i].parentNode.removeChild(item[i]);
     }
 }
-
-
-
-
-// function makeUL(foodItems) {
-//     // Create the list element:
-//     var list = document.createElement('ul');
-
-//     for (var i = 0; i < foodItems.length; i++) {
-//         // Create the list item:
-//         var item = document.createElement('li');
-
-//         // Set its contents:
-//         item.appendChild(document.createTextNode(foodItems[i]));
-
-//         // Add it to the list:
-//         list.appendChild(item);
-//     }
-
-//     // Finally, return the constructed list:
-//     return list;
-// }
-
-
-// var totalPrice = [];
-
-
-// var total = document.createElement("ul");
-// total.innerHTML = "Total"
-// document.body.appendChild(total);
-// document.getElementById('total').appendChild(li);
-
-
-
-
-
-
