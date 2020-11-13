6cf2985dec11ec6e4c383cfd2201ff58112853be
nckmightcs <cmcknight408@gmail.com>
2020-11-12 19:02:13 -0500

    changed desktop view

--- Order/order-script.js
+++ Order/order-script.js
@@ -3,26 +3,48 @@ var foodItems = [];
 
 function selectFoodItem (itemId) {
     var checkbox = document.getElementById(itemId);
+    var list = document.getElementById('total');
     console.log(checkbox.checked)
     if(checkbox.checked) {
+        list.innerHTML = "";
         foodItems.push(checkbox.value)
         console.log(foodItems)
-        var list = document.createElement('ul');
+        // var list = document.createElement('ul');
         for (var i = 0; i < foodItems.length; i++) {
             var item = document.createElement('li');
-            item.appendChild(document.createTextNode(foodItems[i]));
+            item.innerHTML = (foodItems[i]);
+            console.log(item);
             list.appendChild(item);
-            list.innerHTML = foodItems[i];
-            document.body.appendChild(list);
+            // list.innerHTML = foodItems[i];
+            // document.body.appendChild(list);
 
         }
         return list;
         
     } else {
+        list.innerHTML = "";
         var index = foodItems.indexOf(checkbox.value);
         foodItems.splice(index, 1);
         console.log(foodItems);
-        var item = document.getElementById('total');
-        item[i].parentNode.removeChild(item[i]);
+        for (var i = 0; i < foodItems.length; i++) {
+            var item = document.createElement('li');
+            item.innerHTML = (foodItems[i]);
+            console.log(item);
+            list.appendChild(item);
+            // list.innerHTML = foodItems[i];
+            // document.body.appendChild(list);
+        }
+        // item[i].parentNode.removeChild(item[i]);
+        console.log(item)
+    }
+}
+
+var total = []
+
+function addTotal (classNames) {
+    var checkbox = document.getElementsByClassName(classNames);
+    if (checkbox.checked) {
+        total.push(checkbox.itemClass);
+        console.log(total);
     }
 }
